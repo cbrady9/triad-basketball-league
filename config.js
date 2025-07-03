7a1e4f1f0ade3b497349f4593506f6595ea3f4d3
Cam B <cameronbrady1@gmail.com>
2025-07-03 13:47:53 -0400

    Added script loading to all pages

---
+++ config.js
@@ -0,0 +1,23 @@
+const SEASON_CONFIGS = {
+    "2025": { // Your first season
+        "PLAYER_STATS_GID": "340059940",
+        "TEAM_STATS_GID": "1640890982",
+        "SCHEDULE_GID": "1956260076",
+        "STANDINGS_GID": "770406970",
+        "PLAYERS_GID": "0",
+        "TEAMS_GID": "2001178505",
+        "GAME_LOG_GID": "Y306053944",
+        "TRANSACTIONS_GID": "548202372"
+    },
+    "2026": { // Your newly duplicated second season
+        "PLAYER_STATS_GID": "1449243249",
+        "TEAM_STATS_GID": "636058703",
+        "SCHEDULE_GID": "2096810957",
+        "STANDINGS_GID": "1736699475",
+        "PLAYERS_GID": "1745355445",
+        "TEAMS_GID": "1866940250",
+        "GAME_LOG_GID": "1933650257",
+        "TRANSACTIONS_GID": "680517263"
+    }
+    // Add more seasons as you create them
+};
